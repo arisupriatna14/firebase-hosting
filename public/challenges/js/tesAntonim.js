875f2a29b0d238bfdecd5e673a7cd0402d9462e1
arisupriatna14 <arisupriatna703@gmail.com>
2018-05-07 23:07:43 +0700

    modified files

--- public/challenges/js/tesAntonim.js
+++ public/challenges/js/tesAntonim.js
@@ -1,87 +1,60 @@
 function checkResult(){
     var sum = 0;
-    answer1 = document.getElementById('d1')
-    answer2 = document.getElementById('b2')
-    answer3 = document.getElementById('a3')
-    answer4 = document.getElementById('a4')
-    answer5 = document.getElementById('d5')
-    answer6 = document.getElementById('a6')
-    answer7 = document.getElementById('c7')
-    answer8 = document.getElementById('b8')
-    answer9 = document.getElementById('c9')
-    answer10 = document.getElementById('d10')
-  
-    //question 1
-    if(answer1.checked == true){
-      sum++
-      document.getElementById('chk1').style.color = 'green';
-    } else {
-      document.getElementById('chk1').style.color = 'red';
-    }
-  
-    if(answer2.checked == true){
-      sum++
-      document.getElementById('chk2').style.color = 'green';
-    } else {
-      document.getElementById('chk2').style.color = 'red';
-    }
-  
-    if(answer3.checked == true){
-      sum++
-      document.getElementById('chk3').style.color = 'green';
-    } else {
-      document.getElementById('chk3').style.color = 'red'
-    }
-  
-    if(answer4.checked == true){
-      sum++
-      document.getElementById('chk4').style.color = 'green';
-    } else {
-      document.getElementById('chk4').style.color = 'red'
-    }
-  
-    if(answer5.checked == true){
-      sum++
-      document.getElementById('chk5').style.color = 'green';
-    } else {
-      document.getElementById('chk5').style.color = 'red'
-    }
+    
 
-    if(answer6.checked == true){
-      sum++
-      document.getElementById('chk5').style.color = 'green';
-    } else {
-      document.getElementById('chk5').style.color = 'red'
-    }
+    // let sum2 = [
+    //   {
+    //     answer: document.getElementById('d1')
+    //   },
+    //   {
+    //     answer: document.getElementById('b2')
+    //   },
+    //   {
+    //     answer: document.getElementById('a3')
+    //   }, 
+    //   {
+    //     answer: document.getElementById('a4')
+    //   },
+    //   {
+    //     answer: document.getElementById('d5')
+    //   },
+    //   {
+    //     answer: document.getElementById('a6')
+    //   },
+    //   {
+    //     answer: document.getElementById('c7')
+    //   },
+    //   {
+    //     answer: document.getElementById('b8')
+    //   },
+    //   {
+    //     answer: document.getElementById('c9')
+    //   },
+    //   {
+    //     answer: document.getElementById('d10')
+    //   }
+    // ]
 
-    if(answer7.checked == true){
-      sum++
-      document.getElementById('chk5').style.color = 'green';
-    } else {
-      document.getElementById('chk5').style.color = 'red'
-    }
+    let sum2 = [
+      document.getElementById('d1'),
+      document.getElementById('b2'),
+      document.getElementById('a3'),
+      document.getElementById('a4'),
+      document.getElementById('d5')
+    ]
 
-    if(answer8.checked == true){
-      sum++
-      document.getElementById('chk5').style.color = 'green';
-    } else {
-      document.getElementById('chk5').style.color = 'red'
+    // for(var a = 0; a < sum2.length; a++){
+    //   if(sum2[a].answer.checked == true){
+    //     sum++
+    //   }
+    // }
+
+    for(var a = 0; a < sum2.length; a++){
+      if(sum2[a].checked == true){
+        sum++
+      }
     }
   
-    if(answer9.checked == true){
-      sum++
-      document.getElementById('chk5').style.color = 'green';
-    } else {
-      document.getElementById('chk5').style.color = 'red'
-    }
-
-    if(answer10.checked == true){
-      sum++
-      document.getElementById('chk5').style.color = 'green';
-    } else {
-      document.getElementById('chk5').style.color = 'red'
-    }
-
     document.getElementById('score').innerHTML = sum;
     
     var percentage = Math.round((sum * 100) / 10);
